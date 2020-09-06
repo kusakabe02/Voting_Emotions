4e936e1e04816c4b232f0a20d6c4a60c8518ab91
kusakabe02 <kusakabe039411@gmail.com>
2020-09-07 00:46:59 +0900

    二次デプロイ用

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        $this->call(VeUsersTableSeeder::class);
-        $this->call(TrendsTableSeeder::class);
-        $this->call(UserTrendsTableSeeder::class);
+        //$this->call(VeUsersTableSeeder::class);
+        //$this->call(TrendsTableSeeder::class);
+        //$this->call(UserTrendsTableSeeder::class);
     }
 }
